8e6d330c93504ee147537220e5558cd7cf110789
Vancl <vancldada@gmail.com>
2020-03-26 08:47:51 +0100

    HW -1 update

--- Ex1/src/com/ExercisesSchool/Main.java
+++ Ex1/src/com/ExercisesSchool/Main.java
@@ -1,13 +1,23 @@
 package com.ExercisesSchool;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-//        Point point0 = new Point();
-//        Point point1 = new Point(4,3);
-//
-//        System.out.println(point1.toString());
-//        System.out.println(point1.getDistanceFromZero());
 
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(new Point(3,4));
+        points.add(new Point(6,4));
+        points.add(new Point(6,0));
+        points.add(new Point(3,0));
+
+        System.out.println(Point.getDistanceBetween(new Point(3,4),new Point(6,4)));
+        System.out.println(Point.getDistanceBetween(new Point(6,4),new Point(6,0)));
+        System.out.println(Point.getDistanceBetween(new Point(6,0),new Point(3,0)));
+        System.out.println(Point.getDistanceBetween(new Point(3,0),new Point(3,4)));
+
+        NShape shape = new NShape(points);
+        System.out.println("Min: " +shape.getMinDistance() + " | Max: " + shape.getMaxDistance());
     }
 }
